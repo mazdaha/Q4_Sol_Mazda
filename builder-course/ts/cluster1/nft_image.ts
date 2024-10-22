412444469bf719c04a849b231629eeb528c7cc79
Mazda Hamdi <mazda.hamdi@meshconnect.com>
2024-10-22 10:46:57 +0200

    nft session

--- builder-course/ts/cluster1/nft_image.ts
+++ builder-course/ts/cluster1/nft_image.ts
@@ -1,8 +1,8 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../../turbin-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import { readFileSync } from "fs"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -16,13 +16,15 @@ umi.use(signerIdentity(signer));
 (async () => {
     try {
         //1. Load image
+        const image = readFileSync("cluster1/generug.png");
+
         //2. Convert image to generic file.
+        const file = createGenericFile(image, "rug.png");
+
         //3. Upload image
-
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const [myUri] = await umi.uploader.upload([file]);
+        console.log("Your image URI: ", myUri);
+        // https://arweave.net/7NLJzKRbQnXz1acEB8rJe1YZXphRT5AMHRLLBDLfyRWp
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
--- builder-course/ts/cluster1/nft_metadata.ts
+++ builder-course/ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../../turbin-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -17,27 +17,28 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        const image = "https://arweave.net/6strkvBXyCJh8eCoXdk2cF7pKq4SgctVdqL43N2X2gmp";
+        const metadata = {
+            name: "MazRug",
+            symbol: "MZDR",
+            description: "Persian!",
+            image,
+            attributes: [
+                {trait_type: 'rarity', value: '1'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your metadata URI: ", myUri);
+        //  https://arweave.net/GyXYk4rX52cvEZF3HtLRwF9krB2qCPix7sTaDWNkieJR
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
